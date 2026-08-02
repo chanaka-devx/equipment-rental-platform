70756ac6355887ae7edcace06ccb2015356e895d
Chanaka Malawige <eg245176@engug.ruh.ac.lk>
2026-08-02 11:43:44 +0530

    version fix

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,7 +1,7 @@
 import * as dotenv from 'dotenv';
 dotenv.config();
 import { NestFactory } from '@nestjs/core';
-import { ValidationPipe, VersioningType } from '@nestjs/common';
+import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 import { ActivityLogInterceptor } from './common/activity-log/activity-log.interceptor';
 import { PrismaService } from './prisma/prisma.service';
@@ -10,12 +10,8 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const prismaService = app.get(PrismaService);
   
-  app.enableVersioning({
-    type: VersioningType.URI,
-    defaultVersion: '1',
-  });
-  app.useGlobalPipes(new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }));
   app.setGlobalPrefix('v1');
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }));
   app.useGlobalInterceptors(new ActivityLogInterceptor(prismaService));
   app.enableCors();
   await app.listen(process.env.PORT ?? 3000);
